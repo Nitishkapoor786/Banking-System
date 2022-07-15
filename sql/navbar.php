cdbfd79179640f7f5318d521ba6410838b903ccb
Nitish <nitishkapoor7860@gmail.com>
2022-07-15 19:49:50 +0530

    first commit

---
+++ sql/navbar.php
@@ -0,0 +1,27 @@
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="index.php" style="color : orange;"><b>INDIAN BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : orange;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : orange;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : orange;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : orange;"><b>Transaction History</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="http://www.linkedin.com/in/nitish-kapoor-213621222" target="_blank" style="color : orange;"><b>Linkdin</b></a>
+                
+              </li>
+         
+          </div>
+       </nav>
